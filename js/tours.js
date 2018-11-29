04e75d97f07ebfaa1b1ffa0a57c9df042af116c1
Shota999 <ashottmarkarian@gmail.com>
2018-11-29 19:07:58 +0400

    Itvet

---
+++ js/tours.js
@@ -0,0 +1,67 @@
+new Vue({
+    el: '#tours',
+    data: {
+        newTask: '',
+        items: [{
+            name: 'Turkey',
+            text: 'Istanbul, Antalya, Ephesus • 8 days Start your Eastern Europe trip from Berlin – one of the most attractive European cities. Head out to Warsaw – the capital of Poland, where you’ll be able to take a guided tour through the city’s places of interests and museums. Enjoy all Prague has to offer. Opt to…',
+            image: 'images/tour_images/pr-1-1024x452.jpg',
+            date: 'April, 14 2017',
+            detail: 'View Details',
+            athor: {
+                id: 1
+            }
+        },
+        {
+            name: 'Spain',
+            text: 'Madrid, Andalucia, Barcelona • 9 days Start your Eastern Europe trip from Berlin – one of the most attractive European cities. Head out to Warsaw – the capital of Poland, where you’ll be able to take a guided tour through the city’s places of interests and museums. Enjoy all Prague has to offer. Opt to…',
+            image: 'images/tour_images/pr-2-1024x452.jpg',
+            date: 'April, 14 2017',
+            detail: 'View Details',
+            athor: {
+                id: 2
+            }
+        },
+        {
+            name: 'United Kingdom',
+            text: 'England, Scotland, Wales • 13 days Start your Eastern Europe trip from Berlin – one of the most attractive European cities. Head out to Warsaw – the capital of Poland, where you’ll be able to take a guided tour through the city’s places of interests and museums. Enjoy all Prague has to offer. Opt to…',
+            image: 'images/tour_images/pr-3-1024x452.jpg',
+            date: 'April, 14 2017',
+            detail: 'View Details',
+            athor: {
+                id: 3
+            }
+        },
+        {
+            name: 'Easten Europe',
+            text: 'Slovenia, Hungary, Poland • 10 days Start your Eastern Europe trip from Berlin – one of the most attractive European cities. Head out to Warsaw – the capital of Poland, where you’ll be able to take a guided tour through the city’s places of interests and museums. Enjoy all Prague has to offer. Opt to…',
+            image: 'images/tour_images/background-39-1920x900-1024x480.jpg',
+            date: 'April, 14 2017',
+            detail: 'View Details',
+            athor: {
+                id: 4
+            }
+        },
+        {
+            name: 'Italy',
+            text: 'Rome, Milan, Venice • 7 days Start your Eastern Europe trip from Berlin – one of the most attractive European cities. Head out to Warsaw – the capital of Poland, where you’ll be able to take a guided tour through the city’s places of interests and museums. Enjoy all Prague has to offer. Opt to…',
+            image: 'images/tour_images/pr-6-1024x452.jpg',
+            date: 'April, 14 2017',
+            detail: 'View Details',
+            athor: {
+                id: 5
+            }
+        },
+        {
+            name: 'Swiss Alps',
+            text: 'Zurich, Geneve, Luzern • 13 days Start your Eastern Europe trip from Berlin – one of the most attractive European cities. Head out to Warsaw – the capital of Poland, where you’ll be able to take a guided tour through the city’s places of interests and museums. Enjoy all Prague has to offer. Opt to…',
+            image: 'images/tour_images/Untitled-2-1024x597.jpg',
+            date: 'April, 14 2017',
+            detail: 'View Details',
+            athor: {
+                id: 6
+            }
+        },
+    ]
+    },
+});
